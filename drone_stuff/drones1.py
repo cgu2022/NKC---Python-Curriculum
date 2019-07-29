2f71cbb54c74e3bfd9669d05497f6e5a069d4a4b
Chris Gu <cgu2022@pingry.org>
2019-07-28 21:46:14 -0400

    Finished drones1

--- drone_stuff/drones1.py
+++ drone_stuff/drones1.py
@@ -1,13 +1,17 @@
 # Link to library documentation: http://dev-support.robolink.com/
-# Helpful link on python tutorials: https://basecamp.robolink.com/cwists/category#products=%5B2%5D&selected_sort_by=alphabetical
+# Helpful link on python tutorials:
+# https://basecamp.robolink.com/cwists/category#products=%5B2%5D&selected_sort_by=alphabetical
+
+import CoDrone #to prevent errors
 
 #######################################################################################
-# D1.1
+# D1.1A
 # Make absolute sure you have the following codes in seperate cells:
 
 # Import the library
 '''
 import CoDrone
+from CoDrone import Color, Mode
 '''
 
 # Imports the library, creates the drone object, pairs the BLE to the drone
@@ -20,8 +24,11 @@ drone.pair(drone.Nearest)
 print("Paired!")
 '''
 
-# Land the drone code
+# ALWAYS NEED THIS IN EVERY PROGRAM:
 '''
+drone.takeoff()
+#Your stuff
+drone.move(1.5, 0, 0, 0, 0) #sleep(1.5)
 drone.land()
 '''
 
@@ -33,26 +40,80 @@ drone.emergency_stop()
 # DO NOT DISCONNECT THE CONTROLLER TO THE COMPUTER OR PRESS THE RESET BUTTON
 # WHILE THE PROGRAM IS RUNNING!
 
+
 #######################################################################################
-# D1.2
+# D1.1
 # Make the drone hover for 4 seconds
 
 
+#######################################################################################
+# D1.2
+# Make the drone go forward for 2 seconds @ 65% powerlevel. Then,
+# make it go backwards
 
 
 #######################################################################################
-# 3.3
-# make inequalities which all evaluate to True using <=, >=, ==, !=
+# D1.3A
+# Make the drone eyes blue, and:
+# For the drone arm LED, start blue at 10, and then through intervals of 10, get to 250
+# Increment the counter every 0.3 seconds. Print the value of blue each time
+
+
+
+#####################################################################################
+# D1.3B
+# Samething as 3A, but while the arm gets bluer, make the green get dimmer.
+# So for the eyes, make the eyes green (starting at 250), and through intervals
+# of 10, reach 10. 
 
 
 #######################################################################################
-# 3.4
-# make inequalities which all evaluate to False using <=, >=, ==, !=
+# D1.4A
+# Make the drone hover up, land, and then 4 times USING LOOPS
+# Display which iteration (count) the program is currently on
+
 
 #######################################################################################
-# 3.5
-# use and, or, not to combine inequalities using ()
-# 1 evaluates to True using "and", 1 evaluates to False using "and"
-# 1 evaluates to True using "or", 1 evaluates to False using "or"
-# 1 evaluates to True using "not", 1 evaluates to False using "not"
-# try to use a variety of >=, ==, !=, <=, <, >
+# D1.4B
+# Samething as 3a, but the first time make the drone however for 0.7 seconds,
+# 2nd time for 1.4, third time for 2.1, etc. 
+# Display how long it will however for each iteration
+
+
+
+#######################################################################################
+# D1.5
+# Ask the user for what direction should the drone go in, for how long, and what power level.
+# For example, the user enters 1, 2, and then 70 (three seperate prompts),
+# the drone will go forward for 2 seconds @ 70% power level.
+# There are six directions: FORWARD, BACKWARD, UP, DOWN, LEFT, and RIGHT
+
+
+############################################################################################
+# D1.6A
+# Make the drone fly in a square
+
+
+
+############################################################################################
+# D1.6A
+# Samething as 6A, for each movement command, change the color.
+# Ex. First command: Arms are blue
+# Second: Arms are green
+# etc.
+
+
+
+
+###############################################################################################
+# D1.7
+# Make the drone fly up for a second at 70% power level. Then, gradually lower the drone down.
+# While you lower the drone, display the current height of the drone.
+# until it reaches 850mm.
+
+
+###############################################################################################
+# D1.8
+# Go explore and have fun!
+
+
--- drone_stuff/drones1answers.py
+++ drone_stuff/drones1answers.py
@@ -11,6 +11,7 @@ import CoDrone #to prevent errors
 # Import the library
 '''
 import CoDrone
+from CoDrone import Color, Mode
 '''
 
 # Imports the library, creates the drone object, pairs the BLE to the drone
@@ -23,8 +24,11 @@ drone.pair(drone.Nearest)
 print("Paired!")
 '''
 
-# Land the drone code
+# ALWAYS NEED THIS IN EVERY PROGRAM:
 '''
+drone.takeoff()
+#Your stuff
+drone.move(1.5, 0, 0, 0, 0) #sleep(1.5)
 drone.land()
 '''
 
@@ -36,19 +40,9 @@ drone.emergency_stop()
 # DO NOT DISCONNECT THE CONTROLLER TO THE COMPUTER OR PRESS THE RESET BUTTON
 # WHILE THE PROGRAM IS RUNNING!
 
-#######################################################################################
-# D1.1B
-# If the drone is upside down, flash some lights and print something to tell us
-# Else, stop the lights
-
-if drone.is_upside_down():
-    drone.arm_strobe()
-    print("UPSIDE DOWN!")
-else:
-    drone.arm_off()
 
 #######################################################################################
-# D1.2
+# D1.1
 # Make the drone hover for 4 seconds
 
 i = 4
@@ -59,12 +53,59 @@ print("Hovering for %i" % i)
 drone.land()
 print("Landing")
 
+#######################################################################################
+# D1.2
+# Make the drone go forward for 2 seconds @ 65% powerlevel. Then,
+# make it go backwards
+
+drone.takeoff()
+drone.go(Direction.FORWARD, 1, 65) 
+drone.move(1.5, 0, 0, 0, 0) #sleep(1.5)
+drone.land()
 
 #######################################################################################
 # D1.3A
-# Make the drone hover up, land, and then 6 times 
+# Make the drone eyes blue, and:
+# For the drone arm LED, start blue at 10, and then through intervals of 10, get to 250
+# Increment the counter every 0.3 seconds. Print the value of blue each time
 
-for i in range(1, 7):
+drone.eye_pattern(Color.Blue, Mode.BLINK, 1)   
+i = 10
+while i < 250:
+    drone.arm_color(50,50,i, 100)
+    drone.move(0.3, 0, 0, 0, 0) #sleep(1.5)
+    print(i)
+    i+=10
+drone.arm_off()
+print("DONE!")
+
+#####################################################################################
+# D1.3B
+# Samething as 3A, but while the arm gets bluer, make the green get dimmer.
+# So for the eyes, make the eyes green (starting at 250), and through intervals
+# of 10, reach 10. 
+
+drone.eye_pattern(Color.Blue, Mode.BLINK, 1)   
+i = 10
+j = 250
+while i < 250:
+    drone.arm_color(50,50,i, 100)
+    drone.eye_pattern(j, 20, 20, Mode.SOLID)
+    drone.move(0.3, 0, 0, 0, 0) #sleep(1.5)
+    print(i)
+    print(j)
+    i+=10
+    j-=10
+drone.arm_off()
+print("DONE!")
+
+#######################################################################################
+# D1.4A
+# Make the drone hover up, land, and then 4 times USING LOOPS
+# Display which iteration (count) the program is currently on
+
+i = 0
+while i < 4:
     drone.takeoff()
     print("Taking off")
     drone.hover(2)
@@ -73,15 +114,106 @@ for i in range(1, 7):
 
 
 #######################################################################################
-# D1.3B
+# D1.4B
 # Samething as 3a, but the first time make the drone however for 0.7 seconds,
-# 2nd time for 1.4, third time for 2.1, etc.
+# 2nd time for 1.4, third time for 2.1, etc. 
+# Display how long it will however for each iteration
 
-for i in range(1, 7):
+i = 0
+while i < 4:
     drone.takeoff()
     print("Taking off")
-    drone.hover(i)
-    print("Hovering for %i" % i)
+    drone.hover(i * 0.7)
+    print("Hovering for %i" % (i * 0.7))
     drone.land()
     print("Landing")
+
+#######################################################################################
+# D1.5
+# Ask the user for what direction should the drone go in, for how long, and what power level.
+# For example, the user enters 1, 2, and then 70 (three seperate prompts),
+# the drone will go forward for 2 seconds @ 70% power level.
+# There are six directions: FORWARD, BACKWARD, UP, DOWN, LEFT, and RIGHT
+
+drone.takeoff()
+x = input("Enter choice: ")
+y = input("Enter for how long: ")
+z = input("Enter power level:")
+if int(x) == 1:
+    drone.go(Direction.FORWARD, y, z) 
+elif int(x) == 2:
+    drone.go(Direction.BACKWARD, y, z)
+elif int(x) == 3:
+    drone.go(Direction.UP, y, z)
+elif int(x) == 4:
+    drone.go(Direction.DOWN, y, z)
+elif int(x) == 5:
+    drone.go(Direction.LEFT, y, z)
+elif int(x) == 6:
+    drone.go(Direction.RIGHT, y, z)
+drone.move(1.5, 0, 0, 0, 0) #sleep(1.5)
+drone.land()
+
+############################################################################################
+# D1.6A
+# Make the drone fly in a square
+
+drone.takeoff()
+drone.go(Direction.FORWARD, 1, 65) 
+drone.move(0.5, 0, 0, 0, 0) #sleep(1.5)
+drone.go(Direction.RIGHT, 1, 65) 
+drone.move(0.5, 0, 0, 0, 0) #sleep(1.5)
+drone.go(Direction.BACKWARD, 1, 65) 
+drone.move(0.5, 0, 0, 0, 0) #sleep(1.5)
+drone.go(Direction.LEFT, 1, 65) 
+drone.move(0.5, 0, 0, 0, 0) #sleep(1.5)
+drone.move(1.5, 0, 0, 0, 0)
+drone.land()
+
+############################################################################################
+# D1.6A
+# Samething as 6A, for each movement command, change the color.
+# Ex. First command: Arms are blue
+# Second: Arms are green
+# etc.
+
+z = 1
+drone.takeoff()
+drone.arm_color(Color.White, 100)
+drone.go(Direction.FORWARD, z, 65) 
+drone.move(0.5, 0, 0, 0, 0) #sleep(1.5)
+drone.arm_color(Color.Azure, 100)
+drone.go(Direction.RIGHT, z, 65) 
+drone.move(0.5, 0, 0, 0, 0) #sleep(1.5)
+drone.arm_color(Color.Yellow, 100)
+drone.go(Direction.BACKWARD, z, 65) 
+drone.move(0.5, 0, 0, 0, 0) #sleep(1.5)
+drone.arm_color(Color.Green, 100)
+drone.go(Direction.LEFT, z, 65) 
+drone.move(0.5, 0, 0, 0, 0) #sleep(1.5)
+drone.move(1.5, 0, 0, 0, 0) #sleep(1.5)
+drone.arm_off()
+drone.land()
+
+
+###############################################################################################
+# D1.7
+# Make the drone fly up for a second at 70% power level. Then, gradually lower the drone down.
+# While you lower the drone, display the current height of the drone.
+# until it reaches 850mm.
+
+drone.takeoff()
+drone.go(Direction.UP, 1, 70)
+print("Height: " + str(drone.get_height()/1000))
+while(drone.get_height() > 850):
+    drone.go(Direction.DOWN, 1, 75)
+    print("Height: " + str(drone.get_height()/1000))
+print("Height: " + str(drone.get_height()/1000))
+drone.move(1.5, 0, 0, 0, 0) #sleep(1.5)
+drone.land()
+
+###############################################################################################
+# D1.8
+# Go explore and have fun!
+
 
