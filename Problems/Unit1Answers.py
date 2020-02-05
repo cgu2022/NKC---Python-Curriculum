f22c603edb4418c64420df91ecfe19d8fa1af7bb
Valaerys <43929018+Valaerys@users.noreply.github.com>
2020-02-05 13:01:30 -0500

    Create Unit1Answers.py

---
+++ Problems/Unit1Answers.py
@@ -0,0 +1,82 @@
+#######################################################################################
+# 1.1
+# Make 1 variable storing a string, another storing an integer, and another storing a boolean.
+
+s = "string"
+i = "integer"
+b = True
+
+#######################################################################################
+# 1.2
+# Create a string variable that holds your name!
+
+my_name = "Emmet"
+
+#######################################################################################
+# 1.3
+# Make 2 boolean variables that are not equal to each other.
+
+bool1 = True
+bool2 = False
+
+#######################################################################################
+# 1.4
+# Below, we have 2 strings: one stores "dog" and the other stores "cat".
+# Concatenate them together and store the result in a new variable.
+
+string1 = "dog"
+string2 = "cat"
+
+string3 = string1 + string2
+
+# WAIT HERE
+
+#######################################################################################
+# 1.5
+# Use a print statement to print whatever you want to the console!
+
+print("Hello!")
+
+#######################################################################################
+# 1.6
+# Use a print statement to print the variable print_this to the console.
+
+print_this = "Success! You printed a variable!"
+print(print_this)
+
+# Wait here
+
+#######################################################################################
+# 1.7
+# Store what is held in the variable var1 in a new variable called var2
+# Then, print out both variables to prove that they are equal.
+
+var1 = 5
+var2 = var1
+print(var1)
+print(var2)
+
+
+#######################################################################################
+# 1.8
+# Print out the sum of a, b, and c!
+
+a = 1
+b = 2
+c = 3
+
+d = a + b + c
+print(d)
+
+#######################################################################################
+# 1.9
+# Print out 2 different variables on the same line!
+
+
+
+#######################################################################################
+# 1.10
+# You are currently reading a comment! Comment out the following code using # 
+# It will no longer run as code!
+
+var123 = 10
