ac98cb6cea5ca41269f4afb2207184d055be5303
swexler22 <54956513+swexler22@users.noreply.github.com>
2020-02-05 09:01:13 -0500

    Update project2.py

--- Projects/project2.py
+++ Projects/project2.py
@@ -1,1 +1,3 @@
-//INSERT FUN AND INTERESTTING PROJECT WITH WHILE LOOPS
+####################################################
+# Challenge Project: Pyramid Sums
+# Link: https://docs.google.com/document/d/1Uvet4du1sMf7ns32WBduvPVbSBOwKk-N8jCDanL4O7c/
