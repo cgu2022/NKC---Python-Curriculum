a86cc2eaf986c55340532bcec66d20e34165b155
swexler22 <54956513+swexler22@users.noreply.github.com>
2020-02-05 09:05:58 -0500

    Create project2_answers.py

---
+++ Projects/project2_answers.py
@@ -0,0 +1,9 @@
+####################################################
+# Challenge Project: Pyramid Sums answer
+# Link: https://docs.google.com/document/d/1Uvet4du1sMf7ns32WBduvPVbSBOwKk-N8jCDanL4O7c/
+n = 10 //(any number they want here)
+sum = 0
+for i in range (0, n):
+  sum = sum+i
+
+print(sum)
