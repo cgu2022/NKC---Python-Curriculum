6779d92dbf6a15113fb58f51f2bd210e06fb24c5
swexler22 <54956513+swexler22@users.noreply.github.com>
2020-02-11 20:13:43 -0500

    Update project2_answers.py

--- Projects/project2_answers.py
+++ Projects/project2_answers.py
@@ -1,7 +1,7 @@
 ####################################################
 # Challenge Project: Pyramid Sums answer
 # Link: https://docs.google.com/document/d/1Uvet4du1sMf7ns32WBduvPVbSBOwKk-N8jCDanL4O7c/
-n = 10 //(any number they want here)
+n = 10 //(any number you want here)
 sum = 0
 for i in range (0, n):
   sum = sum+i
