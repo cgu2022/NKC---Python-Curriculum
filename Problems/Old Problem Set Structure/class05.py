817e779dd943fa48477668e0e59be07af80d9e5b
Chris Gu <cgu2022@pingry.org>
2020-02-23 18:42:33 -0500

    Re-organized problem sets

---
+++ Problems/Old Problem Set Structure/class05.py
@@ -0,0 +1,61 @@
+#######################################################################################
+# 5.1
+# Make an infinite loop. (But DON'T RUN IT)
+
+
+
+#STOP HERE
+
+#######################################################################################
+# 5.2
+# Make a while loop which prints out the numbers 0 through 10. Here you will need to
+# create a counter variable.
+
+
+#######################################################################################
+# 5.3
+# Make a while loop which counts up by 2s to 100. Print out the current count each loop.
+
+
+#######################################################################################
+# 5.4
+# Make a while loop which counts down by 3s while the count is greater than 0. Print out
+# the count each loop. Start the countdown at 100.
+
+
+ # STOP HERE
+
+#######################################################################################
+# 5.5
+# Use the input function to continually ask the user for a number and add that number
+# to the total. Each loop you should print out the current total. If the user enters
+# -1 the loop stops.
+
+
+#######################################################################################
+# 5.6
+# Make a while loop which has 3 variables and changes each one at a different rate. Stop
+# the while loop when a variable gets to 100.
+
+a = 0
+b = 0
+c = 0
+
+ # STOP HERE
+
+#######################################################################################
+# 5.7
+# Make a while loop within a while loop (Nested While Loop). In the outer while loop count up to 10 from 1
+# and in the inner loop count up to 3 from 1. Make sure to always reset the 2nd counter
+# for each iteration of the first while loop
+
+
+#######################################################################################
+# 5.8
+# Make a while loop that goes from 0 to 1000. If the counter variable equals 100, break
+# out of the loop. Use the break statement for this.
+
+
+#######################################################################################
+# 5.9
+# Make a string and use the length function to print out the length of the string
---
+++ Problems/Unit 3/Unit3Set3.py
@@ -0,0 +1,84 @@
+##################################################################################
+# 6.2.1 Create a program using a while loop which asks the user for an integer until that integer is -1. 
+# Whenever the integer is not -1, multiply the input by the total.
+#
+# total = 1;
+#
+#
+#
+##################################################################################
+# 6.2.2 Create a while loop with the condition of True that adds 3 to a total 
+# and breaks when the total reaches a multiple of 9.
+#
+# total = 0
+#
+#
+#
+##################################################################################
+# 6.2.3 Create a program with a nested while loop. 
+# The outside while loop should iterate 4 times.
+# The inside loop should iterate 10 times.
+# The outside loop should use the variable x and the inside loop should use the variable y. 
+# Print out the values of x and y for each iteration of the inside
+#
+#
+#
+##################################################################################
+# 6.2.4 Create a program which prints out the following output.
+# For this problem we must use two variables, x and y both set to 5. 
+#
+# 1 2 3 4 5
+# 1 2 3 4
+# 1 2 3
+# 1 2
+# 1
+#
+#
+#
+##################################################################################
+# 6.2.5 Make a while loop that creates this pattern:
+#
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+#
+#
+#
+##################################################################################
+# Enter 7 temperatures from the keyboard – each on representing a
+# daily temperature for the previous week. Sum the 7 values then calculate and print the average
+# temperature for the week. 
+#
+# 
+#
+
+###################################################################################
+'''
+Write a program that asks a user to guess a secret number between 1 and N, where N is a
+positive number that the user is prompted for. The program should also prompt the user for
+the maximum number of guesses they would like to make. Each time the player makes a guess,
+the program shall respond with “correct”, “too low”, or “too high”. The program should keep
+track of the number of guesses the user made to discover the secret number. The program
+should continue execution until the user has discovered the secret number or has exceeded the
+maximum number of guesses. The program shall also allow the user to play the game again
+until the user declines.
+'''
+
+##################################################################################
+# Joe just graduated college and has been offered his first real job as a widget
+# designer at Widgets USA, Inc. He is disappointed in the offered beginning salary; 
+# only $20,000 per year. But he is promised a 10% raise each year. 
+# Other job opportunities offer more to start, but have no promised raises. 
+# To help Joe decide, print out a table of his new salary each year until his salary
+# reaches or exceeds $50,000. (How many years will it take?) 
+# The table is to look as follows. Print your output to a file then turn in the print-out.
+#
+# Year    Salary
+# -------------------
+# 1       $ 20,000.00
+# 2       $ 22,000.00
+# etc...
+#
+
