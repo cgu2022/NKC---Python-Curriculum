92469340e5621ee524545f71ecfb790c5aa3e1a8
Chris Gu <cgu2022@pingry.org>
2020-06-19 01:23:37 -0400

    Added More Stuff

--- Problems/Unit 3/Unit3Set1.py
+++ Problems/Unit 3/Unit3Set1.py
@@ -23,22 +23,13 @@
 # the count each loop. Start the countdown at 100.
 
 
- # STOP HERE
+
 
 #######################################################################################
 # 5.5
 # Make a while loop which has 3 variables and changes each one at a different rate. Stop
 # the while loop when a variable gets to at least 100.
 
-a = 0
-b = 0
-c = 0
-
-while a < 100 and b < 100 and c < 100:
-    print("a: {} b: {} c: {}".format(a, b, c))
-    a += 1
-    b += 2
-    c += 3
 
  # STOP HERE
 
@@ -48,13 +39,8 @@ while a < 100 and b < 100 and c < 100:
 # to the total. Each loop you should print out the current total. If the user enters
 # -1 the loop stops.
 
-total = 0
-inp = int(input())
-while not inp == -1:
-	print("{} + {} = {}".format(total, inp, total+inp))
-	total = total + inp
-	inp = int(input())
 
+ # STOP HERE
 
 #######################################################################################
 # 5.7
---
+++ Problems/Unit 3/mathproject(withoutfunctions).py
@@ -0,0 +1,47 @@
+import random as rand
+
+numOfCorrect = 0
+numOfIncorrect = 0
+print("Welcome to the arithmetic quiz!")
+print("Enter q at any time to quit.")
+print("Round down when doing division:")
+finished = False
+
+while not finished:
+    num1 = rand.randint(1, 100)
+    num2 = rand.randint(1, 100)
+    operation = rand.randint(0, 5)
+    correctAns = 0
+    ans = 0
+    if operation == 0:
+        ans = input(str(num1) + " + " + str(num2) + " = ")
+        correctAns = num1 + num2
+    elif operation == 1:
+        ans = input(str(num1) + " - " + str(num2) + " = ")
+        correctAns = num1 - num2
+    elif operation == 2:
+        ans = input(str(num1) + " * " + str(num2) + " = ")
+        correctAns = num1 * num2
+    else:
+        smallest = min(num1, num2)
+        largest = max(num1, num2)
+        num1 = largest
+        num2 = smallest % 11
+        if num2 == 0:
+            num2 = 1
+        ans = input(str(num1) + " / " + str(num2) + " = ")
+        correctAns = int(num1 / num2)
+    if ans == "q":
+        finished = True
+        total = numOfCorrect + numOfIncorrect
+        print( "Number of Correct:", str(float(numOfCorrect / (total) * 100)) + "%", "(" + str(numOfCorrect) + "/" + str(total) + ")")
+        print( "Number of Incorrect:", str(float(numOfIncorrect / (total) * 100)) + "%", "(" + str(numOfIncorrect) + "/" + str(total) + ")")
+    else:
+        ans = int(ans)
+        if ans == correctAns:
+            print("Correct!")
+            numOfCorrect += 1
+        else:
+            print("Incorrect!")
+            numOfIncorrect += 1
+print("Bye Bye!")
